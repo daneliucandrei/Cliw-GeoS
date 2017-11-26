9cecda2dc09f69649a2a6ef539fcb6537ca45f51
gabrielmiron15 <miron.gabriel2015@gmail.com>
2017-11-26 20:37:47 +0200

    header scroll , cum functioneaza

--- js/general.js
+++ js/general.js
@@ -1,6 +1,5 @@
-console.log(' aici');
+console.log('se incarca');
 var $=document;
-console.log($);
 function onHover(selector)
 {
     $.getElementsByClassName(selector)[0].setAttribute("src", "media/earth-spinning-rotating-animation-25.gif");
@@ -9,4 +8,14 @@ function onHover(selector)
 function offHover(selector)
 {
     $.getElementsByClassName(selector)[0].setAttribute("src", "media/earth-spinning-rotating-animation-25-0.png");
+}
+
+window.onscroll=function()
+{ var header=$.getElementsByClassName('header-container')[0];
+    if (window.scrollY>290) {
+        header.classList+= " header-container-scroll";
+    }
+    else {
+        header.classList.remove("header-container-scroll");
+    }
 }
