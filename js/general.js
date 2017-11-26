0941b37bed2a1235e6a4b22f23d3264c352f1bfc
gabrielmiron15 <miron.gabriel2015@gmail.com>
2017-11-26 21:43:48 +0200

    merge with Daneliuc

--- js/general.js
+++ js/general.js
@@ -1,19 +1,18 @@
 console.log('se incarca');
-var $=document;
-function onHover(selector)
-{
+var $ = document;
+function onHover(selector) {
     $.getElementsByClassName(selector)[0].setAttribute("src", "media/earth-spinning-rotating-animation-25.gif");
 }
 
-function offHover(selector)
-{
+function offHover(selector) {
     $.getElementsByClassName(selector)[0].setAttribute("src", "media/earth-spinning-rotating-animation-25-0.png");
 }
 
-window.onscroll=function()
-{ var header=$.getElementsByClassName('header-container')[0];
-    if (window.scrollY>290) {
-        header.classList+= " header-container-scroll";
+window.onscroll = function () {
+    var header = $.getElementsByClassName('header-container')[0];
+    if (window.scrollY > 290) {
+        if (!header.classList.contains('header-container-scroll'))
+            header.classList += " header-container-scroll";
     }
     else {
         header.classList.remove("header-container-scroll");
