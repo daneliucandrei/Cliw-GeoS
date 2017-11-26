20a73e3d7a9c21981ea8f9fa0b43ac6f5ad02182
gabrielmiron15 <miron.gabriel2015@gmail.com>
2017-11-26 15:30:27 +0200

    footer

--- js/general.js
+++ js/general.js
@@ -1,1 +1,12 @@
 console.log(' aici');
+var $=document;
+console.log($);
+function onHover(selector)
+{
+    $.getElementsByClassName(selector)[0].setAttribute("src", "media/earth-spinning-rotating-animation-25.gif");
+}
+
+function offHover(selector)
+{
+    $.getElementsByClassName(selector)[0].setAttribute("src", "media/earth-spinning-rotating-animation-25-0.png");
+}
