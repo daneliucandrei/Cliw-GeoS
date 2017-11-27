9f44ecc78c2230bfe4c67e62201f144601130e54
gabrielmiron15 <miron.gabriel2015@gmail.com>
2017-11-27 21:38:22 +0200

    map filters and geolocation

--- js/general.js
+++ js/general.js
@@ -10,11 +10,32 @@ function offHover(selector) {
 
 window.onscroll = function () {
     var header = $.getElementsByClassName('header-container')[0];
-    if (window.scrollY > 290) {
+    if (window.scrollY > 150) {
         if (!header.classList.contains('header-container-scroll'))
             header.classList += " header-container-scroll";
     }
     else {
         header.classList.remove("header-container-scroll");
     }
+}
+
+function toggleNext(thisId) {
+   var next= $.getElementById(thisId).nextElementSibling;
+   if(!next.classList.contains('filter-toggle--active')) {
+       next.classList+= " filter-toggle--active";
+   }
+   else {
+       next.classList.remove("filter-toggle--active");
+   }
+}
+function toggleThis(thisId){
+    var $this= $.getElementById(thisId);
+    console.log($this.classList);
+    if(!$this.classList.contains('button-filter--active')) {
+        $this.classList+= " button-filter--active";
+    }
+    else {
+        $this.classList.remove("button-filter--active");
+    }
+
 }
