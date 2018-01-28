6226319d51f127c41dc38b4fd2ca34b5c93b3ea2
Ilie Andrei Daneliuc <daneliucilieandrei@gmail.com>
2018-01-28 20:48:28 +0200

    Resolve Bug Continent Filter for Flickr

--- js/map.js
+++ js/map.js
@@ -80,7 +80,9 @@ function CreatePopUpMarker(marker, content) {
 
 function CreateUrlFlickr(dataFilter) {
     setMapOnAll(null, flickrMarkers);
-    flickrMarkers = [];   
+    flickrMarkers = [];
+    let url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d2eab41b196d9ba760b21e0b3004b48d&has_geo=1&extras=geo&per_page=500&page=1&format=json&nojsoncallback=1';
+    
     var continents = dataFilter.geo;
     url = url + '&bbox=';
     switch(continents) {
