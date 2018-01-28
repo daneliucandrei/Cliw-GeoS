189ab2a92b9fb635698a53a5fc20311f77d7de51
vdsalex <voaides.alex@gmail.com>
2018-01-28 14:21:27 +0200

    Inserted microdata.

--- js/storage.js
+++ js/storage.js
@@ -48,66 +48,45 @@ var ApplyFilter = {
     features: {
         'popular': 'popular',
         'today': 'today',
-        'yesterday':'fresh_yesterday',
-        'week':'fresh_week'
+        'yesterday': 'fresh_yesterday',
+        'week': 'fresh_week'
     }
     ,
     camera: {}
+    ,
+    geo: {
+        'america_n_input':'44.330,-109.754,3715.679km',
+        'america_s_input':'-23.030,-67.903,3902.238km',
+        'antarctica_input':'-81.500,0.000,1883.860km',
+        'africa_input':'2.070,15.800,4623.261km',
+        'asia_input':'34.969,99.819,3733.764km',
+        'australia_input':'-30.941,140.810,2334.124km',
+        'europa_input': '52.976,7.857,1923.322km',
+    }
 };
 var dataFilterAbstract = {};
 dataFilterAbstract.rpp = 50;
 dataFilterAbstract.only = '';
-dataFilterAbstract.image_size =200;
+dataFilterAbstract.image_size = 200;
 dataFilterAbstract.was_featured_type = '';
+var boolCenter = false;
 mapFilter.add = function (key, value) {
     this.values[key] = value;
     return this;
 };
-
-function initializeMapFilter() {
-    if (read_cookie(appName) === null) {
-        console.log('not cookie');
-        for (var key in list) {
-            if (typeof(list[key].id) === "string") {
-                entity = list[key].id;
-                mapFilter.add(entity, list[key].checked);
-            }
-        }
-    }
-    else {
-        console.log('cookie');
-        mapFilter.values = read_cookie(appName);
-        var dataFilter = dataFilterAbstract;
-        for (var key in mapFilter.values) {
-            if (mapFilter.values[key]) {
-                (document.getElementById(key).checked = true);
-                if (typeof ApplyFilter.category[key] !== 'undefined') {
-                    dataFilter.only += ApplyFilter.category[key] + ',';
-                }
-                if (typeof ApplyFilter.features[key] !== 'undefined') {
-                    dataFilter.was_featured_type = ApplyFilter.features[key];
-                }
-                if (typeof ApplyFilter.image_size[key] !== 'undefined') {
-                    dataFilter.image_size = ApplyFilter.image_size[key];
-                }
-
-            }
-        }
-        if (mapFilter.values['500px_input']) {
-            window.onload = function () {
-                createMap500px(dataFilter, true)
-            }
-        }
+function triggerEvent(el, type)
+{
+    if ((el[type] || false) && typeof el[type] == 'function')
+    {
+        el[type](el);
     }
 }
-
-initializeMapFilter();
-
 function watchStorage(list) {
     var key;
     for (key in list) {
         if (typeof(list[key].id) === "string") {
-            list[key].onclick = function () {
+            list[key].onchange = function () {
+                console.log('change',this.id);
                 var dataFilter = dataFilterAbstract;
                 mapFilter.values[this.id] = this.checked;
                 push_cookie(appName, mapFilter.values);
@@ -122,13 +101,25 @@ function watchStorage(list) {
                         if (typeof ApplyFilter.image_size[i] !== 'undefined') {
                             dataFilter.image_size = ApplyFilter.image_size[i];
                         }
+                        if (typeof ApplyFilter.geo[i] !== 'undefined') {
+                            dataFilter.geo = ApplyFilter.geo[i];
+                            var location = ApplyFilter.geo[i].split(',');
+                            zoomMap({lat: parseFloat(location[0]), lng: parseFloat(location[1])}, 3);
+                            boolCenter = true;
+                        }
+                        if(!boolCenter) {
+                            zoomMap(defaultPos,2);
+                        }
                     }
                 }
+                appliedFilters(list);
                 if (mapFilter.values['500px_input']) {
                     createMap500px(dataFilter, true);
                     dataFilter.only = '';
                     dataFilter.was_featured_type = '';
-                    dataFilter.image_size=200;
+                    dataFilter.image_size = 200;
+                    dataFilter.geo = '';
+                    boolCenter=false;
                 }
                 if (this.id === '500px_input') {
                     if (this.checked) {
@@ -142,5 +133,83 @@ function watchStorage(list) {
         }
     }
 }
+function appliedFilters(list,removeAll) {
+    var filterActive;
+    document.getElementById('filter-active').innerHTML = "";
+    if(typeof removeAll=== 'undefined') {
+        removeAll = false;
+    }
+    if (!removeAll) {
+        for (var key in list) {
+            if (typeof(list[key].id) === "string") {
+                if (list[key].checked)
+                    if (typeof list[key].dataset.section !== 'undefined') {
+                        console.log(list[key].dataset.section);
+                        document.getElementById('filter-active').innerHTML += '<div class="top-filter-active" data-id=' + list[key].id + '>' + list[key].dataset.section + '</div>';
+                    }
+            }
+        }
+    }
+    else {
+        for (var key in list) {
+            if(list[key].checked)
+            {
+                document.getElementById(list[key].id).checked=false;
+                triggerEvent(document.getElementById(list[key].id), 'onchange');
 
-watchStorage(list);
+            }
+        }
+    }
+}
+window.onload = function () {
+    function initializeMapFilter() {
+        if (read_cookie(appName) === null) {
+            console.log('not cookie');
+            for (var key in list) {
+                if (typeof(list[key].id) === "string") {
+                    entity = list[key].id;
+                    mapFilter.add(entity, list[key].checked);
+                }
+            }
+        }
+        else {
+            console.log('cookie');
+            mapFilter.values = read_cookie(appName);
+            var dataFilter = dataFilterAbstract;
+            for (var key in mapFilter.values) {
+                if (mapFilter.values[key]) {
+                    (document.getElementById(key).checked = true);
+                    if (typeof ApplyFilter.category[key] !== 'undefined') {
+                        dataFilter.only += ApplyFilter.category[key] + ',';
+                    }
+                    if (typeof ApplyFilter.features[key] !== 'undefined') {
+                        dataFilter.was_featured_type = ApplyFilter.features[key];
+                    }
+                    if (typeof ApplyFilter.image_size[key] !== 'undefined') {
+                        dataFilter.image_size = ApplyFilter.image_size[key];
+                    }
+                    if (typeof ApplyFilter.geo[key] !== 'undefined') {
+                        dataFilter.geo = ApplyFilter.geo[key];
+                        var location = ApplyFilter.geo[key].split(',');
+                        zoomMap({lat: parseFloat(location[0]), lng: parseFloat(location[1])}, 3);
+                        boolCenter = true;
+                    }
+
+
+                }
+            }
+            appliedFilters(list);
+            if (mapFilter.values['500px_input']) {
+                createMap500px(dataFilter, true)
+            }
+        }
+    }
+
+    initializeMapFilter();
+
+    document.getElementById('button-remove-filtre').onclick = function () {
+        appliedFilters(list,true);
+    };
+
+    watchStorage(list);
+}
